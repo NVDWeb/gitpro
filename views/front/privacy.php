737038478d40eaf498b2045340e719a51530a465
Ashraf ali <ali.ashraf191@gmail.com>
2017-11-27 12:28:32 +0530

    changes

--- views/front/privacy.php
+++
@@ -1,5 +0,0 @@
-<div class="terms_page">
-	<div class="container">
-    	<?php defined('BASEPATH') OR exit('No direct script access allowed');?>
-        <?php echo $privacy[0]->description;?>
-</div></div>
--- views/front/terms.php
+++
@@ -1,6 +0,0 @@
-<div class="terms_page">
-	<div class="container">
-			<?php defined('BASEPATH') OR exit('No direct script access allowed');?>
-        <?php echo $terms[0]->description;?>
-    </div>
-</div>
